87e5a03b9ccc179935ffb4951639226d7a8640a9
AbdulKarim018 <lackhorse.ak@gmail.com>
2024-03-14 21:01:03 +0500

    arithmetic and BMI calculator

---
+++ Typescript/calculator/src/index.ts
@@ -0,0 +1,79 @@
+import inquirer from "inquirer";
+
+const whatToCalculate = await inquirer.prompt({
+  type: "list",
+  name: "whatToCalculate",
+  message: "What do you want to calculate?",
+  choices: ["BMI (Body Mass Index)", "Arithmatic Calculator"],
+});
+
+if (whatToCalculate.whatToCalculate === "BMI (Body Mass Index)") {
+  // BMI Calculator
+
+  const weightAndHeight = await inquirer.prompt([
+    {
+      type: "number",
+      name: "weight",
+      message: "Enter your weight in kg",
+    },
+    {
+      type: "number",
+      name: "height",
+      message: "Enter your height in m",
+    },
+  ]);
+
+  // Calculate BMI
+  const bmi =
+    weightAndHeight.weight / (weightAndHeight.height * weightAndHeight.height);
+
+  // Display BMI
+  console.log(`Your BMI is ${bmi}`);
+} else {
+  // Arithmatic Calculator
+
+  const operation = await inquirer.prompt({
+    name: "operation",
+    type: "list",
+    message: "What operation do you want to perform?",
+    choices: ["Add", "Subtract", "Multiply", "Divide", "Modulus", "Exponent"],
+  });
+
+  // Get User's Numbers
+  const numbers = await inquirer.prompt([
+    {
+      type: "number",
+      name: "firstNumber",
+      message: "Enter first number",
+    },
+    {
+      type: "number",
+      name: "secondNumber",
+      message: "Enter second number",
+    },
+  ]);
+
+  let result;
+
+  switch (operation.operation) {
+    case "Add":
+      result = numbers.firstNumber + numbers.secondNumber;
+      break;
+    case "Subtract":
+      result = numbers.firstNumber - numbers.secondNumber;
+      break;
+    case "Multiply":
+      result = numbers.firstNumber * numbers.secondNumber;
+      break;
+    case "Divide":
+      result = numbers.firstNumber / numbers.secondNumber;
+      break;
+    case "Modulus":
+      result = numbers.firstNumber % numbers.secondNumber;
+      break;
+    case "Exponent":
+      result = numbers.firstNumber ** numbers.secondNumber;
+      break;
+  }
+  console.log(`The result is ${result}`);
+}
